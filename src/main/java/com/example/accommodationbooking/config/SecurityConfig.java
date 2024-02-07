20f973611bec0c36c8d3fafd703993860463e555
Denys-Maksym Kachalup <denys.k82@gmail.com>
2024-02-07 12:54:43 +0200

    added configuration for swagger

--- src/main/java/com/example/accommodationbooking/config/SecurityConfig.java
+++ src/main/java/com/example/accommodationbooking/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -57,5 +58,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getPassEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/v3/api-docs/**",
+                "/swagger-ui.html", "/swagger-ui/**");
     }
 }
