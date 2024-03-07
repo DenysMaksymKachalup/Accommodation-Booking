1fc66f50b93eb7ecff9875989a1c00beae0b41c1
Denys-Maksym Kachalup <denys.k82@gmail.com>
2024-03-07 21:34:46 +0200

    fixed checkstyle in user model

--- src/main/java/com/example/accommodationbooking/model/User.java
+++ src/main/java/com/example/accommodationbooking/model/User.java
@@ -39,6 +39,7 @@ public class User implements UserDetails {
             joinColumns = @JoinColumn(name = "users_id"),
             inverseJoinColumns = @JoinColumn(name = "roles_id"))
     private Set<Role> roles;
+
     public User(Long id) {
         this.id = id;
     }
